8ef534869395289cbb611a61dd769821451adc69
Blessmi K <kblessmi2001@gmail.com>
2023-01-13 18:44:24 +0530

    palindrome problem finished

---
+++ palindrome.js
@@ -0,0 +1,14 @@
+let str="nrupul";
+let string="";
+  for(let i=N;i<=0;i--)
+  {
+      string+=str[i];
+  }
+  if(str=string)
+  {
+      console.log("Yes")
+  }
+  else
+  {
+      console.log(No)
+  }
